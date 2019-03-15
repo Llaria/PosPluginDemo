385688f974a1f51b47e1b8fbfc6959303c8cb1c5
Sundy <dsun17@best-inc.com>
2019-03-15 16:39:31 +0800

    插件测试

--- app/src/main/java/sun/sundy/posplugindemo/MainActivity.java
+++ app/src/main/java/sun/sundy/posplugindemo/MainActivity.java
@@ -1,5 +1,6 @@
 package sun.sundy.posplugindemo;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,30 +12,52 @@ import com.qihoo360.replugin.model.PluginInfo;
 
 import java.io.File;
 
+import kr.co.namee.permissiongen.PermissionFail;
+import kr.co.namee.permissiongen.PermissionGen;
+import kr.co.namee.permissiongen.PermissionSuccess;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PermissionGen
+                .with(this)
+                .addRequestCode(0x13)
+                .permissions(
+                        Manifest.permission.ACCESS_NETWORK_STATE,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.RECEIVE_BOOT_COMPLETED,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.MODIFY_AUDIO_SETTINGS,
+                        Manifest.permission.ACCESS_WIFI_STATE,
+                        Manifest.permission.CHANGE_WIFI_STATE,
+                        Manifest.permission.READ_PHONE_STATE
+                )
+                .request();
     }
 
     public void open(View view) {
         Intent intent = RePlugin.createIntent("sun.sundy.pospluginone",
                 "sun.sundy.pospluginone.PluginOneActivity");
-        if (!RePlugin.startActivity(this, intent)){
+        if (!RePlugin.startActivity(MainActivity.this, intent)){
             ToastUtils.showLazzToast("启动失败！");
         }
     }
 
     public void install(View view) {
-        String pluginApk = "pluginone.apk";
+        String pluginApk = "pospluginone-debug.apk";
+//        String pluginApk = "app-release.apk";
         String fileName = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + pluginApk;
         System.out.println("======fileName:" + fileName);
         File pluginFile = new File(fileName);
         //文件不存在就返回
         if (!pluginFile.exists()){
             System.out.println("=====不存在");
+            ToastUtils.showLazzToast("不存在");
             return;
         }
         PluginInfo info = null;
@@ -44,7 +67,9 @@ public class MainActivity extends AppCompatActivity {
         if (info != null){
             //预先加载
             RePlugin.preload(info);
-            ToastUtils.showLazzToast("安装 pluginone 成功 " + info.getName());
+            ToastUtils.showLazzToast("安装 pospluginone-debug 成功 " + info.getName());
+        }else {
+            ToastUtils.showLazzToast("安装失败！");
         }
     }
 
@@ -54,6 +79,23 @@ public class MainActivity extends AppCompatActivity {
         }else {
             ToastUtils.showLazzToast("卸载失败");
         }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        PermissionGen.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
+    }
+
+    //权限申请成功
+    @PermissionSuccess(requestCode = 0x13)
+    public void doSomething() {
+
+    }
+
+    //申请失败
+    @PermissionFail(requestCode = 0x13)
+    public void doFailSomething() {
 
     }
 }
--- app/src/main/java/sun/sundy/posplugindemo/PluginApplication.java
+++ app/src/main/java/sun/sundy/posplugindemo/PluginApplication.java
@@ -1,16 +1,55 @@
 package sun.sundy.posplugindemo;
 
 import android.app.Application;
+import android.content.Context;
 
+import com.qihoo360.replugin.RePlugin;
 import com.qihoo360.replugin.RePluginApplication;
+import com.qihoo360.replugin.RePluginConfig;
 
 public class PluginApplication extends RePluginApplication{
 
     public static Application app;
 
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        //-------------- 开发的时候不验证签名 -----------
+//        RePlugin.App.attachBaseContext(this,
+//                new RePluginConfig()
+//                        .setVerifySign(false)
+//                        .setPrintDetailLog(true)
+//                        .setUseHostClassIfNotFound(true)
+//                        .setMoveFileWhenInstalling(false));
+        // FIXME 允许接收rpRunPlugin等Gradle Task，发布时请务必关掉，以免出现问题
+        RePlugin.enableDebugger(base, BuildConfig.DEBUG);
+    }
+
+    @Override
+    protected RePluginConfig createConfig() {
+        RePluginConfig c = new RePluginConfig();
+
+        // 允许“插件使用宿主类”。默认为“关闭”
+        c.setUseHostClassIfNotFound(true);
+
+        // FIXME RePlugin默认会对安装的外置插件进行签名校验，这里先关掉，避免调试时出现签名错误
+        c.setVerifySign(!BuildConfig.DEBUG);
+
+        // 针对“安装失败”等情况来做进一步的事件处理
+//        c.setEventCallbacks(new HostEventCallbacks(this));
+
+        // FIXME 若宿主为Release，则此处应加上您认为"合法"的插件的签名，例如，可以写上"宿主"自己的。
+        // RePlugin.addCertSignature("AAAAAAAAA");
+
+        // 在Art上，优化第一次loadDex的速度
+        // c.setOptimizeArtLoadDex(true);
+        return c;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
         app = this;
+        RePlugin.App.onCreate();
+        RePlugin.enableDebugger(this,true);
     }
 }
